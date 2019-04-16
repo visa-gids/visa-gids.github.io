4ecefdfda69e1c50ae536c10c427a9ff274db4d8
visa-gids <49696419+visa-gids@users.noreply.github.com>
2019-04-17 01:32:56 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+var cacheName = 'weatherPWA-step-6-2-rm';
+var filesToCache = [
+  '/',
+  '/qr-jss.js',
+  '/mani-skills.png'
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+});
+
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
